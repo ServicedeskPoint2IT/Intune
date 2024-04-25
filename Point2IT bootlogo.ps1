46f969ae8e4e57a9157c3787a23ff5db336c4eb8
ServicedeskPoint2IT <167416859+ServicedeskPoint2IT@users.noreply.github.com>
2024-04-25 13:11:46 +0200

    Add files via upload

---
+++ Point2IT bootlogo.ps1
@@ -0,0 +1,20 @@
+﻿
+$FolderPath = "C:\Beheer"
+ 
+If(Get-ChildItem -Path $FolderPath -Force -ErrorAction SilentlyContinue)
+{
+    Write-Host "Folder already Exists!"
+}
+else
+{
+    #PowerShell Create directory if not exists
+    New-Item $FolderPath -ItemType Directory
+     
+    Write-Host "New Folder Created!"
+}
+
+$source = "https://raw.githubusercontent.com/ServicedeskPoint2IT/WinDeBloat/main/Point2IT%20logo.jpeg"
+$destination = "C:\Beheer\Point2ITboot.jpeg"
+Invoke-WebRequest $source -OutFile $destination
+
+Set-HPFirmwareBootLogo -file C:\Beheer\Point2ITboot.jpeg
