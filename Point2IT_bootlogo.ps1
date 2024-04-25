458092d9fd9b164f336f5280c2ed72b202ed1c74
ServicedeskPoint2IT <167416859+ServicedeskPoint2IT@users.noreply.github.com>
2024-04-25 14:02:31 +0200

    Update Point2IT_bootlogo.ps1

--- Point2IT_bootlogo.ps1
+++ Point2IT_bootlogo.ps1
@@ -1,3 +1,18 @@
+##Elevate if needed
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host "                                               3"
+    Start-Sleep 1
+    Write-Host "                                               2"
+    Start-Sleep 1
+    Write-Host "                                               1"
+    Start-Sleep 1
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`" -WhitelistApps {1}" -f $PSCommandPath, ($WhitelistApps -join ',')) -Verb RunAs
+    Exit
+}
+
 $FolderPath = "C:\Bootlogo"
  
 If(Get-ChildItem -Path $FolderPath -Force -ErrorAction SilentlyContinue)
